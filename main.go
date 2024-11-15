e02058f5826994e5804548c043688b8395d3c006
r0bert <robertdsgabriel@gmail.com>
2024-11-15 10:23:54 -0300

    replace main.go logic to iterate over a list of names and print each one; add hello.txt with greeting

--- main.go
+++ main.go
@@ -2,28 +2,9 @@ package main
 
 import "fmt"
 
-var( 
-	nome string
-	n1 int
-	n2 int32
-)
-func main(){
-	mensagem := "Aula de Go"
-	fmt.Println(mensagem)
-
-	var b, f, s = true, 2.3, "Olá"
-	fmt.Println(b, f, s)
-
-	var total int
-	total++
-	fmt.Println("total:", total)
-
-	nome = "r0bert"
-	fmt.Println("Hello", nome, "!")
-
-	var x,y = 10, 20
-	fmt.Println(x, y)
-	x, y = y, x
-	fmt.Println(x, y)
-
+func main() {
+nomes := []string{"João", "Maria", "José", "Pedro", "Ana"}
+for _, nome := range nomes {
+	fmt.Println(nome)
+}
 }
