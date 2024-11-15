b2adaf0b02d9d0d1b6525aabca4a53e57a08bd78
r0bert <robertdsgabriel@gmail.com>
2024-11-15 01:13:06 -0300

    refactor main.go to include additional variables and print statements

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
-var nome string
-
+var( 
+	nome string
+	n1 int
+	n2 int32
+)
 func main(){
-nome = "r0bert"
-fmt.Println("Hello", nome, "!")
+	mensagem := "Aula de Go"
+	fmt.Println(mensagem)
+
+	var b, f, s = true, 2.3, "Olá"
+	fmt.Println(b, f, s)
+
+	var total int
+	total++
+	fmt.Println("total:", total)
+
+	nome = "r0bert"
+	fmt.Println("Hello", nome, "!")
+
+	var x,y = 10, 20
+	fmt.Println(x, y)
+	x, y = y, x
+	fmt.Println(x, y)
 
 }
