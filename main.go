58fa77548c7ae1f5643335b7224d428c57023ac2
r0bert <robertdsgabriel@gmail.com>
2024-11-19 10:09:02 -0300

    add Pessoa and Categoria structs; update main function to create and print a Pessoa instance

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
-nomes := []string{"João", "Maria", "José", "Pedro", "Ana"}
-for _, nome := range nomes {
-	fmt.Println(nome)
+type Pessoa struct {
+	Nome      string
+	Sobrenome string
+	Idade     uint8
+	Status    bool
 }
+
+type Categoria struct {
+	Nome string
+	Pai  string
+}
+
+func main() {
+	p := Pessoa{
+		Nome:      "Robert",
+		Sobrenome: "Araújo",
+		Idade:     27,
+		Status:    true,
+	}
+
+	fmt.Println(p, p.Nome)
 }
